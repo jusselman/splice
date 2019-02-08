5566f175c04c6e752386c0a5a7fb44500c083f3d
Joshua Usselman <usselmanjosh@gmail.com>
2019-02-07 22:47:43 -0800

    slice v split

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var string1 = "Hola Mundo";
+
+var string2 = string1.slice(3, 8);
+
+console.log(string2);
+
+var string3 = string1.slice(3);
+
+console.log(string3);
+
+var categ = "clothing, food, religion, philosophy, character";
+
+var categArr = categ.split(',');
+
+console.log(cateArr);
